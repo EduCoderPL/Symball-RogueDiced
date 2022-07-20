83587c1780484184f13b33a5b87e09a37573a8f5
AMFeniksPL <arkadiuszmazurek13@gmail.com>
2022-07-21 00:09:33 +0200

    Code clean

--- Assets/Scripts/PlayerMechanics/PlayerMovement.cs
+++ Assets/Scripts/PlayerMechanics/PlayerMovement.cs
@@ -48,7 +48,6 @@ public class PlayerMovement : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        //rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
         rb.velocity += moveSpeed * Time.fixedDeltaTime * movement;
     }
     public void PlayerHit(HitEventData data)
